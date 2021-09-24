ad9ef78ae751e63642f6c2ee0da614b19642f84a
unknown <samucarp99@gmail.com>
2021-09-23 21:08:17 -0300

    Contenido de estudios :hammer:

--- src/components/Estudios/Cartas/CartasContainer.js
+++ src/components/Estudios/Cartas/CartasContainer.js
@@ -1,27 +1,30 @@
 import React from 'react';
 import { Cartas } from './Cartas';
 
-export const CartasContainer = ({id, title, image, description, more, web, javascript}) => {
+export const CartasContainer = ({id, title, image, description, more, web, javascript, vista = ""}) => {
+
+
 
     return(
         <div className="container-fluid">
             <div className="col-12 mt-5" id={id}>
-                <h2 className="col-12 text-center mb-5">{title}</h2>
+                <h2 className="col-12 text-center mb-5 text-decoration-underline">{title}</h2>
                 <div className="container d-flex justify-content-between align-items-center">
                     <img src={image} alt={title} className="col-4 imageStudy"/>
                     <p className="col-6 fs-5">{description}</p>
-                </div>
+                </div>                
                 <p className="col-12 fs-5">{more}</p>                        
             </div>  
 
             
-            {/* Carruseles */}
+            {/* Card */}
             <div className="container d-flex justify-content-between">
-                {/* para carrusel de Platzi Desarrollo Web*/}
+                {/* para Cards de Platzi Desarrollo Web*/}
                 <div className="col-4">
                     <h3 className="text-center">Desarrollo Web</h3>
                     <Cartas array={web}/>
                 </div>
+                {/* para Cards de Platzi Javascript*/}
                 <div className="col-4">
                     <h3 className="text-center">Javascript</h3>
                     <Cartas array={javascript}/>
--- src/components/Estudios/SoloEstudios.js
+++ src/components/Estudios/SoloEstudios.js
@@ -53,17 +53,36 @@ export const SoloEstudios = () => {
                 {lerned.map((study) =>  study.title === 'Maquetador Web' 
 
                 ?
+                
                 <CartasContainer {...study}/> 
 
                 :
 
-                /* Sin carrusel */
+                /* Sin Cards */
 
                 <div className="col-12 mt-5" id={study.id}>
-                    <h2 className="text-center mb-5">{study.title}</h2>
+                    <h2 className="text-center mb-5 text-decoration-underline">{study.title}</h2>
                     <div className="container d-flex justify-content-between align-items-center">
                         <img src={study.image} alt={study.title} className="col-4 imageStudy"/>
                         <p className="col-6 fs-5">{study.description}</p>
+                    </div>
+                    <div>
+                        {study.vista ? 
+                        <div className="container">
+
+                            <div className="d-flex align-items-center justify-content-center">
+                                <img src={study.vista} alt={study.title} className="col-4 gifImage"/>
+                            </div>
+
+                            <div className="container d-flex align-items-center justify-content-center">
+                                <a href={study.repositorio} target="_blank"><button type="button" className="btn btn-info repositorio">Visitar Repositorio</button></a>
+                            </div>
+
+                        </div>
+                        : 
+                        <></>
+                        }
+                        
                     </div>
                     <p className="col-12 fs-5">{study.more}</p>                        
                 </div>                
