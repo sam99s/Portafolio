5f0939b7c3b47b1a8bd5931f85fa215a8ef051f5
sam99s <samuel.schulz.dev@gmail.com>
2022-05-15 20:59:53 -0300

    fix proximos estudios :bug:

--- src/components/Estudios/Proximos.js
+++ src/components/Estudios/Proximos.js
@@ -1,46 +1,53 @@
-import React, { useState } from 'react';
-import { db } from '../../firebase/config';
-import './Proximos.scss'
+import React, { useState } from "react";
+import { Fragment } from "react/cjs/react.production.min";
+import { db } from "../../firebase/config";
+import "./Proximos.scss";
 
 export const Proximos = () => {
+  const [pronto, setPronto] = useState([]);
 
-    const [pronto,setPronto] = useState([]);
+  const othersStudies = async () => {
+    try {
+      const snapshot = db.collection("proximos");
+      await snapshot.get().then((response) => {
+        const data = response.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }));
 
-    const othersStudies = async() => {
-
-        try{
-            const snapshot = db.collection('proximos');
-            await snapshot.get()
-                .then((response) => {
-                    const data = response.docs.map((doc) => ({
-                        ...doc.data(),
-                        id: doc.id
-                    }))
-                    
-                    setPronto(data)
-                })
-
-        } catch(err){
-            console.log('Error: ', err)
-        }
-
+        setPronto(data);
+      });
+    } catch (err) {
+      console.log("Error: ", err);
     }
+  };
 
-    if(pronto.length === 0){
-        othersStudies()
-    }
+  if (pronto.length === 0) {
+    othersStudies();
+  }
 
-    return(
+  return (
+    <Fragment>
+      {pronto.length ? (
         <div className="container">
-            <h3 className="text-center corte">¿Qué sigo estudiando?</h3>
-            {pronto.map((study) => <div className="col-12 mt-5" key={study.id}>
-                <h2 className="text-center">{study.title}</h2>
-                <div className="container d-flex justify-content-between align-items-center">
-                    <p className="col-6 fs-5">{study.description}</p>
-                    <img src={study.image} alt={study.title} className="col-4 imageStudy"/>
-                </div>
-            </div>)}
+          <h3 className="text-center corte">¿Qué sigo estudiando?</h3>
+          {pronto.map((study) => (
+            <div className="col-12 mt-5" key={study.id}>
+              <h2 className="text-center">{study.title}</h2>
+              <div className="container d-flex justify-content-between align-items-center">
+                <p className="col-6 fs-5">{study.description}</p>
+                <img
+                  src={study.image}
+                  alt={study.title}
+                  className="col-4 imageStudy"
+                />
+              </div>
+            </div>
+          ))}
         </div>
-    )
-    
-}
+      ) : (
+        <></>
+      )}
+    </Fragment>
+  );
+};
